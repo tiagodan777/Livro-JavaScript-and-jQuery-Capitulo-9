02df7ee1ba36bc22848e5b5dd48c63dacc8a250b
Tiago Daniel <187806573+tiagodan777@users.noreply.github.com>
2026-04-25 19:01:07 +0000

    ok

--- c09/js/history.js
+++ c09/js/history.js
@@ -1,1 +1,28 @@
-// ESTE EXEMPLO SÓ FUNCIONA SE FOR NUM SERVIDOR, POR ISSO, VOU CRIAR UMA PASTA NO MAMP
+$(function() {
+    function loadContent(url) {
+        $('#content').load(url + ' #content').hide().fadeIn('fast');
+    }
+
+    $('nav a').on('click', function(e) {
+        e.preventDefault();
+
+        var href = this.href;
+        var $this = $(this);
+
+        $('a.current').removeClass('current');
+        $this.addClass('current');
+
+        loadContent(href);
+
+        history.pushState('', $this.text, href);
+    });
+
+    window.onpopstate = function() {
+        var path = location.pathname;
+        var page = path.substring(path.lastIndexOf('/') + 1);
+        loadContent(path);
+        
+        $('a.current').removeClass('current');
+        $('[href="' + page + '"]').addClass('current');
+    }
+})
